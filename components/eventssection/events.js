069a8ac36e13c7b35e711f28c8f74a18941abed8
Gary <58466698+garypach@users.noreply.github.com>
2021-10-17 15:20:59 -0700

    events section dynamic

--- components/eventssection/events.js
+++ components/eventssection/events.js
@@ -1,22 +1,35 @@
 import Image from "next/image";
 import Link from 'next/link';
+import { useEffect, useState } from "react";
 const Events = () => {
+
+const eventimages = [
+    {id:'1', imageName:'family-gathering-mobile.jpg',tag:'family', head:'Family Gathering',text:'We love catering for entire families. So please bring everyone along for a special meal with your loved ones. We’ll provide a memorable experience for all.'},
+    {id:'2', imageName:'special-events-mobile.jpg',tag:'special', head:'Special Events',text:'Whether it’s a romantic dinner or special date you’re celebrating with others we’ll look after you. We’ll be sure to mark your special date with an unforgettable meal.'},
+    {id:'3', imageName:'social-events-mobile.jpg',tag:'social', head:'Social Events',text:'Are you looking to have a larger social event? No problem! We’re more than happy to cater for big parties. We’ll work with you to make your event a hit with everyone.'},
+]
+const[tag,setTag] = useState(0);
+    const[selectedImage,setSelectedImage] = useState([]);
+    useEffect(()=>{
+        tag === 0 ? setSelectedImage(eventimages[0]) : setSelectedImage(eventimages[tag])
+    },[tag])
+   
+
     return(
         <div className="events-cont">
             <div className="event-cont">
             <div className="event-img">
-            <Image src={`/homepage/family-gathering-mobile.jpg`}  width="326px" height="400px" alt={`family`}/>
+            <Image src={`/homepage/${selectedImage.imageName}`}  width="326px" height="400px" alt={`${selectedImage.tag}`}/>
             </div>
             <div className="event-txt-cont">
                 <div className="events">
-                    <div> Family Gathering</div>
-                    <div> Special Events </div>
-                    <div> Social Events</div>
+                <TagImage number={0} handleSetTag={setTag} text={'Famliy Gatherings'} tagActive={tag === 0 ? true : false }/>
+                <TagImage number={1} handleSetTag={setTag} text={'Special Events'} tagActive={tag === 1? true : false }/>
+                <TagImage number={2} handleSetTag={setTag} text={'Social Events'} tagActive={tag === 2? true : false }/>
                 </div>
                 <div className="event-txt">
-                    <h1>Family Gathering</h1>
-                    <p>We love catering for entire families. So please bring everyone along for a special meal with your loved ones. We’ll provide a memorable experience for all.</p>
-                   
+                    <h1>{selectedImage.head}</h1>
+                    <p>{selectedImage.text}</p>                   
                 </div>
                 <Link href="/book">
                     <a>
@@ -28,6 +41,9 @@ const Events = () => {
         </div>
       
     )
+}
+const TagImage = ({number,handleSetTag,tagActive,text}) =>{
+    return <div className={`tag ${tagActive ? 'active' : null}`} onClick={ () => handleSetTag(number)}><span>{text}</span><div className={`event-line ${tagActive ? 'event-line-active' : null}`}></div></div>
 }
 
 export default Events;
